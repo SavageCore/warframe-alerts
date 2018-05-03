2ab710ccdc6e869c8188d7865c7d486ecbabc280
SavageCore <talk@savagecore.eu>
2018-05-03 15:16:23 +0100

    feat: support different enviroments in `build/start.js`

--- build/start.js
+++ build/start.js
@@ -1,9 +1,11 @@
 const childProcess = require('child_process');
 const electron = require('electron');
 const webpack = require('webpack');
+const mri = require('mri');
 const config = require('./webpack.app.config');
 
-const env = 'development';
+const args = mri(process.argv.slice(2));
+const env = args.env ? args.env : 'development';
 const compiler = webpack(config(env));
 let electronStarted = false;
 
