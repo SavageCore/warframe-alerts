27efaefd9cf1adf1a539c93437e8f2b09b3ba4a6
SavageCore <talk@savagecore.eu>
2019-01-21 13:17:43 +0000

    Fix gulp

--- tasks/start.js
+++ tasks/start.js
@@ -4,7 +4,7 @@ const childProcess = require('child_process');
 const electron = require('electron');
 const gulp = require('gulp');
 
-gulp.task('start', ['build', 'watch'], () => {
+gulp.task('start', gulp.series('build', 'watch'), () => {
 	childProcess.spawn(electron, ['.'], {stdio: 'inherit'})
 		.on('close', () => {
 			// User closed the app. Kill the host process.
